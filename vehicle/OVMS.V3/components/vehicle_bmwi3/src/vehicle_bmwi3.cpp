8784aca4a8681d5de1812ce44417acb76b607160
eroobeeroo <andrew@dowdellfamily.com>
2025-01-18 15:55:52 -0500

    Update vehicle_bmwi3.cpp
    commented out

--- vehicle/OVMS.V3/components/vehicle_bmwi3/src/vehicle_bmwi3.cpp
+++ vehicle/OVMS.V3/components/vehicle_bmwi3/src/vehicle_bmwi3.cpp
@@ -346,13 +346,13 @@ void OvmsVehicleBMWi3::Ticker1(uint32_t ticker)
 }
 OvmsVehicle::vehicle_command_t OvmsVehicleBMWi3::CommandClimateControl(bool climatecontrolon)
 {
-    OvmsVehicle::vehicle_command_t command;
+    //OvmsVehicle::vehicle_command_t command;
           ESP_LOGI(TAG, "CommandClimateControl");
         SendPreconditioningCommand();  // Call the method to send the CAN message
         
  
 
-    return command;  // Return the updated command
+   // return command;  // Return the updated command
 }
 void OvmsVehicleBMWi3::Ticker10(uint32_t ticker)
 {
